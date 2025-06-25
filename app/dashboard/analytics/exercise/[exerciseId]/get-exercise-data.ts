aedc90f03b49b7266b617494fe00180ed8de4a65
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2025-06-25 18:09:30 +0000

    Add Get Exercise Data for Analytics Page

---
+++ app/dashboard/analytics/exercise/[exerciseId]/get-exercise-data.ts
@@ -0,0 +1,92 @@
+"use server"
+
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
+import { and, eq } from "drizzle-orm"
+
+import { db } from "@/db/db"
+import { 
+  exercise, 
+  exerciseInstance, 
+  exerciseType, 
+  setInstance,
+  timeDistanceInstance,
+  weightRepsInstance, 
+  workoutInstance 
+} from "@/db/schema"
+import { auth } from "@/lib/auth"
+import { ExerciseData } from "./page"
+
+export const getExerciseData = async (id: string): Promise<ExerciseData | void> => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session) {
+    redirect("/sign-in")
+  }
+
+  const userId = session.user.id
+
+  const exerciseRes = await db
+    .select({
+      name: exercise.name,
+      type: exerciseType.name,
+      userId: exercise.userId
+    })
+    .from(exercise)
+    .innerJoin(exerciseType, eq(exercise.typeId, exerciseType.id))
+    .where(eq(exercise.id, id))
+
+  if (exerciseRes.length === 0) {
+    return
+  }
+
+  if (exerciseRes[0].userId !== userId) {
+    redirect("/403")
+  }
+
+  const exerciseData: ExerciseData = {
+    name: exerciseRes[0].name,
+    type: exerciseRes[0].type as ExerciseData["type"],
+    sets: []
+  }
+
+  if (exerciseData.type === "weightReps") {
+    const sets = await db
+      .select({
+        weight: weightRepsInstance.weight,
+        reps: weightRepsInstance.reps,
+        date: workoutInstance.date
+      })
+      .from(weightRepsInstance)
+      .innerJoin(setInstance, eq(weightRepsInstance.setInstanceId, setInstance.id))
+      .innerJoin(exerciseInstance, eq(setInstance.exerciseInstanceId, exerciseInstance.id))
+      .innerJoin(workoutInstance, eq(exerciseInstance.workoutInstanceId, workoutInstance.id))
+      .where(and(
+        eq(exerciseInstance.exerciseId, id),
+        eq(setInstance.completed, true)
+      ))
+    
+    exerciseData.sets = sets
+  } else {
+    const sets = await db
+      .select({
+        time: timeDistanceInstance.time,
+        distance: timeDistanceInstance.distance,
+        date: workoutInstance.date
+      })
+      .from(timeDistanceInstance)
+      .innerJoin(setInstance, eq(timeDistanceInstance.setInstanceId, setInstance.id))
+      .innerJoin(exerciseInstance, eq(setInstance.exerciseInstanceId, exerciseInstance.id))
+      .innerJoin(workoutInstance, eq(exerciseInstance.workoutInstanceId, workoutInstance.id))
+      .where(and(
+        eq(exerciseInstance.exerciseId, id),
+        eq(setInstance.completed, true)
+      ))
+    
+    exerciseData.sets = sets
+  }
+
+  return exerciseData
+}
--- app/dashboard/analytics/exercise/[exerciseId]/page.tsx
+++ app/dashboard/analytics/exercise/[exerciseId]/page.tsx
@@ -1,9 +1,41 @@
+import { getExerciseData } from "./get-exercise-data"
+
+type TimeDistanceExerciseData = {
+  name: string
+  type: "timeDistance"
+  sets: {
+    time: number,
+    distance: number,
+    date: string
+  }[]
+}
+
+type WeightRepsExerciseData = {
+  name: string
+  type: "weightReps"
+  sets: {
+    weight: number
+    reps: number
+    date: string
+  }[]
+}
+
+export type ExerciseData = TimeDistanceExerciseData | WeightRepsExerciseData
+
 export default async function ExercisePage({
   params
 }: {
   params: Promise<{ exerciseId: string }>
 }) {
   const { exerciseId } = await params
+
+  const data = await getExerciseData(exerciseId)
+
+  if (!data) {
+    return (
+      <div>Exercise does not exist. It may have been deleted.</div>
+    )
+  }
 
   return (
     <>{exerciseId}</>
